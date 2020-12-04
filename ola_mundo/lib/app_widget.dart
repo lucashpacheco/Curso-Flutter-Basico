c92b123685eafba3b233584909d327060c9fa3db
Lucas Pacheco <lucashpmendonca@gmail.com>
2020-12-04 16:48:33 -0300

    Introdução a mudança d estado global , dark theme

--- ola_mundo/lib/app_widget.dart
+++ ola_mundo/lib/app_widget.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
+import 'app_controller.dart';
+
 import 'home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          primarySwatch: Colors.lightBlue, brightness: Brightness.dark),
-      home: HomePage(),
-    );
+    return AnimatedBuilder(
+        animation: AppController.instance,
+        builder: (context, child) {
+          return MaterialApp(
+            theme: ThemeData(
+              primarySwatch: Colors.lightBlue,
+              brightness: AppController.instance.isDartTheme
+                  ? Brightness.dark
+                  : Brightness.light,
+            ),
+            home: HomePage(),
+          );
+        });
   }
 }
--- ola_mundo/lib/home_page.dart
+++ ola_mundo/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ola_mundo/app_controller.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,23 +10,19 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   int counter = 0;
+  bool isDartTheme = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
       ),
-      body: Container(
-        height: 200,
-        width: 200,
-        color: Colors.black,
-        child: Align(
-          alignment: Alignment.center,
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.green,
-          ),
+      body: Center(
+        child: Switch(
+          value: AppController.instance.isDartTheme,
+          onChanged: (value) {
+            AppController.instance.changeTheme();
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
