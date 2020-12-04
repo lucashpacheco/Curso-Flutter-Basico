e51881dab8eea3122fdc94fec6b51ee7ec34fff0
Lucas Pacheco <lucashpmendonca@gmail.com>
2020-12-04 16:08:15 -0300

    Reorganização(Intro-States)

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -1,47 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app_widget.dart';
+
 main() {
-  runApp(AppWidget(
-    title: 'Hello World!!',
-  ));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({Key key, this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.lightBlue),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  int counter = 0;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: GestureDetector(
-          child: Text('Contador :$counter'),
-          onTap: () {
-            setState(() {
-              counter++;
-            });
-          },
-        ),
-      ),
-    );
-  }
+  runApp(AppWidget());
 }
