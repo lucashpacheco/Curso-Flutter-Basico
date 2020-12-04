09ed0296f282d03ab05062197595f9bf6b38b329
Lucas Pacheco <lucashpmendonca@gmail.com>
2020-12-04 17:07:40 -0300

    Colunas , alinhamentos

--- ola_mundo/lib/home_page.dart
+++ ola_mundo/lib/home_page.dart
@@ -16,13 +16,20 @@ class HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
+        actions: [
+          CustomSwitch(),
+        ],
       ),
-      body: Center(
-        child: Switch(
-          value: AppController.instance.isDartTheme,
-          onChanged: (value) {
-            AppController.instance.changeTheme();
-          },
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('Contador: $counter'),
+            CustomSwitch(),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -33,6 +40,18 @@ class HomePageState extends State<HomePage> {
           });
         },
       ),
+    );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: AppController.instance.isDartTheme,
+      onChanged: (value) {
+        AppController.instance.changeTheme();
+      },
     );
   }
 }
